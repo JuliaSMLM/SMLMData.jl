7bb3369ed6fe82f90d9a5afef09293d622ffaf40
Documenter.jl <documenter@juliadocs.github.io>
2021-10-18 17:28:36 +0000

    build based on 1eb1b5b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMLMData","category":"page"},{"location":"#SMLMData","page":"Home","title":"SMLMData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SMLMData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMLMData]","category":"page"}]
+}
