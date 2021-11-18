a2d0d080d0669354299d1d3c3f9c4e8107729cc3
dschodt <35635770+dschodt@users.noreply.github.com>
2021-11-17 18:10:33 -0700

    Revise usage of a method of isolatesmld()

--- src/isolatesmld.jl
+++ src/isolatesmld.jl
@@ -1,32 +1,6 @@
 using Base
 
 """
-    smld_sub = isolatesmld(smld::SMLD2D, subind::UnitRange)
-
-Isolate the `smld` localizations specified by `subind` indices.
-
-# Description
-This method grabs the localizations indexed by `subind` from `smld` and outputs
-a new SMLD2D structure containing only those localization.
-"""
-function isolatesmld(smld::SMLD2D, subind::UnitRange)
-    # Loop through each field and add it to the output SMLD.  For scalar
-    # fields, we don't have to apply the indexing of `subind`.
-    fields = fieldnames(SMLMData.SMLD2D)
-    nfields = Base.length(fields)
-    smld_sub = deepcopy(smld)
-    for ii = 1:nfields
-        currentfield = getfield(smld, fields[ii])
-        if isa(currentfield, Vector) && any(fields[ii] .== smld.datafields)
-            # If this field is a vector, we'll keep only the `subind` elements.
-            setfield!(smld_sub, fields[ii], currentfield[subind])
-        end
-    end
-
-    return smld_sub
-end
-
-"""
     smld_sub = isolatesmld(smld::SMLD2D, subind::Vector{Int})
 
 Isolate the `smld` localizations specified by `subind` indices.
@@ -76,6 +50,19 @@ function isolatesmld(smld::SMLD2D, keepbit::BitVector)
     end
 
     return smld_sub
+end
+
+"""
+    smld_sub = isolatesmld(smld::SMLD2D, subind::UnitRange)
+
+Isolate the `smld` localizations specified by `subind` indices.
+
+# Description
+This method grabs the localizations indexed by `subind` from `smld` and outputs
+a new SMLD2D structure containing only those localization.
+"""
+function isolatesmld(smld::SMLD2D, subind::UnitRange)
+    return SMLMData.isolatesmld(smld, collect(subind))
 end
 
 """
