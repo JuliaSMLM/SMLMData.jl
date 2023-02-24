376594010815f00c2e87176e96e136a533ffd7a7
sajjad88 <56313813+sajjad88@users.noreply.github.com>
2023-02-24 10:54:26 -0700

    Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,10 @@ using Test
 
 @testset "SMLMData.jl" begin
     # Write your tests here.
+    
+    ## Create an empty SMLD2D structure.
+    smld_empty = SMLMData.SMLD2D()
+    ## Create an empty SMLD2D structure with a specified numberr of localizations.
+    nlocalizations = 100
+    smld_init = SMLMData.SMLD2D(nlocalizations)
 end
