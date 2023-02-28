4050ddb9494e857d86c80e369f880769c358d1e5
sajjad88 <56313813+sajjad88@users.noreply.github.com>
2023-02-27 22:12:42 -0700

    Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,8 @@ using Test
     ## Create an empty SMLD2D structure.
     smld_empty = SMLMData.SMLD2D()
     ## Create an empty SMLD2D structure with a specified numberr of localizations.
-    nlocalizations = 100
+    nlocalizations = 10
     smld_init = SMLMData.SMLD2D(nlocalizations)
+    smld_init.bg[1] = 10
+    @test isapprox(sum(smld_init.bg),10.0; atol=1e-6)
 end
